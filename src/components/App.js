ce734c6dfd8b93349c57d67480d195a76be63a18
Anjan <anjanlayek001@gmail.com>
2022-11-05 18:41:21 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,17 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+const onChange=(e)=>{
+  console.log(e);
+}
+const onClick = (e) =>{
+  let text = document.getElementById('input').value;
+  document.getElementById('text').innerHTML = `Hello my name is ${text} and I study at Newton School`
+}
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
+      <input id='input' onChange={onChange}></input>
+      <button id='button' onClick={onClick}>Click</button>
       <p id='text'> Hello my name is ____ and I study at Newton School</p>
     </div>
   )
